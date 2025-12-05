c6d4905c6f8be465c7e9c4d3cd3c97110ea2bd5a
Sourav Chongrey <souravchongrey@gmail.com>
2025-12-05 20:49:12 +0530

    add list

---
+++ src/javabasic/datastructure/ArrayListTest.java
@@ -0,0 +1,12 @@
+package javabasic.datastructure;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+     cars.add("maruti");
+     cars.add("toyoto");
+        System.out.println(cars);
+    }
+}
---
+++ src/javabasic/datastructure/LinkListTest.java
@@ -0,0 +1,14 @@
+package javabasic.datastructure;
+
+import java.util.LinkedList;
+
+public class LinkListTest {
+    static void main() {
+        LinkedList<String> cars = new LinkedList<String>();
+           cars.add("volvo");
+           cars.add("Maruti");
+           cars.add("jip");
+        System.out.println(cars);
+
+    }
+}
