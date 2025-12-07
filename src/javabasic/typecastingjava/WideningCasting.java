c275850b8a9b7ba41cd5624c78067bcb5500e678
Sourav Chongrey <souravchongrey@gmail.com>
2025-12-07 12:40:14 +0530

    widening Casting

---
+++ src/javabasic/typecastingjava/WideningCasting.java
@@ -0,0 +1,10 @@
+package javabasic.typecastingjava;
+
+public class WideningCasting {
+    static void main(String[] args) {
+        int myInt = 9; // Integer
+        double myDouble = myInt; //  double
+        System.out.println(myInt);
+        System.out.println(myDouble); //Automatic casting : int to double
+    }
+}
