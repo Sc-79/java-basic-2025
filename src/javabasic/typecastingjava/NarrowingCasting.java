720c535b0ea3a0f174acc26f4b9d529dcac06f7e
Sourav Chongrey <souravchongrey@gmail.com>
2025-12-07 12:54:57 +0530

    Narrowing

---
+++ src/javabasic/typecastingjava/NarrowingCasting.java
@@ -0,0 +1,12 @@
+package javabasic.typecastingjava;
+
+public class NarrowingCasting {
+
+    static void main(String[] args) {
+        double myDouble = 9.023d;
+        int myInt = (int)myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+
+}
